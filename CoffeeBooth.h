8d89de17790ca8a0df64b2816cdfbaac821af17c
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-12-09 19:28:15 -0800

    10 minute timestamp

--- CoffeeBooth.h
+++ CoffeeBooth.h
@@ -19,9 +19,11 @@ private:
         string drinkName;
     };
 
+    // Representing a Customer
+    Customer customer;
     // Customer queue
     list<Customer> queue;
-
+    // Vectors and arrays to populate customer info
     vector<string> firstNames;
     vector<string> lastNames;
     const string drinkList[8] = {"Tea", "Coffee", "Sprite", "Milk", "Orange Juice", "Pepsi", "Grape Drink", "Pocari Sweat"};
@@ -29,9 +31,19 @@ private:
 public:
     CoffeeBooth() {}
     // Constructor with name vectors to dynamically create customers
-    CoffeeBooth(const vector<string> &fNames, const vector<string> &lNames) {
+    CoffeeBooth(const vector<string> &fNames, const vector<string> &lNames)
+    {
         firstNames = fNames;
         lastNames = lNames;
+    }
+
+    void addRandCustomer()
+    {
+        // Create customer name and drink
+        customer.name = firstNames.at(rand() % firstNames.size()) + lastNames.at(rand() % lastNames.size());
+        customer.drinkName = drinkList[rand() % 8];
+
+        queue.push_back(customer);
     }
 
     // Setters and getters
